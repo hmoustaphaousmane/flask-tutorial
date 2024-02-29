1139e1b72a4ab98c41dfd901d2eebfbf444ac277
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-29 12:22:34 +0100

    feat: creating functions that will run the SQL command of schema.sql

--- flaskr/db.py
+++ flaskr/db.py
@@ -24,3 +24,20 @@ def close_db(e=None):
     # If the connection existes (g.db is set)
     if db is not None:
         db.close()  # Close it
+
+
+def init_db():
+    """Function that runs the script of schema.sql."""
+    db = get_db()  # Get a database connection
+
+    with current_app.open_resource('schema.sql') as f:  # Open the file schema
+        db.executescript(f.read().decode('utf8'))  # Execute the commands read
+        # from the file `scheman.sql`
+
+
+# Define a new command line called `init-db` that calls the init_bd function
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initilized the database.')
