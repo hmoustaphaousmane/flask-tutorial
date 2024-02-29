3bf66b6f96d8f01343d104232871d79c5ac59dac
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-29 10:41:29 +0100

    chore: start creating the application factory function

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,21 @@
 # This file will serve two purposes. First, it thells Python that the `flaskr`
 # directory should be treated as a package. Second it will contain the app
 # factory.
+
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """
+    Appliction factory function
+    """
+
+    # Create and configure the app
+
+    app = Flask(__name__, instance_relative_config=True)
+    # __name__: the name of the current Python module (import name)
+    # The second argument tells the app that configuration files are relative
+    # to the instance folder
+
+    return app
