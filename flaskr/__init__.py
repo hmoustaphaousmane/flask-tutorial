0489f9023b6989925e18b801c84633f631218d5d
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-02-29 10:45:44 +0100

    chore: set secret key and database default configurations

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -14,8 +14,15 @@ def create_app(test_config=None):
     # Create and configure the app
 
     app = Flask(__name__, instance_relative_config=True)
-    # __name__: the name of the current Python module (import name)
+    # __name__: the name of the current Python module (import name - root path)
     # The second argument tells the app that configuration files are relative
     # to the instance folder
+
+    # Set some default configurations
+    app.config.from_mapping(
+        SECRET_KEY='dev',  # Used by Flask and extension to keep data safe
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')  # Path where
+        # the SQLite database file will be saved
+    )
 
     return app
