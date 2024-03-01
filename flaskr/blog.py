1c03522056c85d1b55b2c7e4d7fb7a5c708d0147
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-03-01 11:29:22 +0100

    feat: create index view and template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,3 +7,16 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__, url_prefix='/blog')
+
+
+@bp.route('/')
+def index():
+    """View that shows all the posts, most recent first"""
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    return render_template('blog/index.html', posts=posts)
