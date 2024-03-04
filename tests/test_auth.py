04e4e657dad1fa4b1bc2eb8ca9ff93eefe3dd42a
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-03-04 15:45:43 +0100

    test: ensure that the register view:       renders successfulle in get and redirects to the login URL       saves the user's data in the database on post with valid from data,       dispaly an error message with invalid data

---
+++ tests/test_auth.py
@@ -0,0 +1,38 @@
+import pytest
+from flaskr import g, session
+from flaskr.db import get_db
+
+
+def test_register(client, app):
+    # Asser that the page renders successfully by making a get request
+    # (client.get makes a GET request and returns the Response object returned
+    # by Flask) and check for a 200 OK status code
+    assert client.get('/auth/register').status_code == 200
+
+    response = client.post(
+        '/auth/register', data={'username': 'a', 'password': 'a'}
+    )
+    assert response.headers["location"] == '/auth/login'
+    # headers will have a Location header with the login URL when the register
+    # view redirects to the login view
+
+    with app.app_context():
+        assert get_db().execute(
+            "SELECT * FROM user WHERE username = 'a'",
+        ).fetchone() is None
+
+
+# Tell Pytest to run the same test function with different arguments thanks to
+# `pytest.mark.parametrize`
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
+    # data contains the body of the response as bytes
