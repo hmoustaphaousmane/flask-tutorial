eea6288d941305f01af9fdfa8d0b5a4a4a8f4111
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-03-04 14:55:27 +0100

    test: ensure that the init-db command calls the init_db function

--- tests/test_db.py
+++ tests/test_db.py
@@ -16,3 +16,33 @@ def test_get_close_db(app):
 
     # Assert that the raised error indicates a closed connection
     assert 'closed' in str(e.value)
+
+
+def test_init_db_command(runner, monkeypatch):
+    """
+    Ensure that `init-db` command calls  the `init_db` function
+    """
+
+    class Recorder(object):
+        """
+        Helper class to track if `init_db` is called without executing it
+        """
+        called = False
+
+    def fake_init_db():
+        """
+        Mock implementation of `init_db` that sets `Recorder.called` to True
+        """
+        Recorder.called = True
+
+    # Monkeypatch the actual init_db function with the fake implementation
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)  # i.e. replace the
+    # init_db function with one that records that it’s been called
+
+    # Invoke the 'init-db' command using the test runner
+    result = runner.invoke(args=['init-db'])
+
+    # Assert successful initialization
+    assert 'Initialized' in result.output
+    # Assert that The init_db function should have been called
+    assert Recorder.called
